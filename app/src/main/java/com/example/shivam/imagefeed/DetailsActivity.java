abb8e3b5aacf03a188575fc10a3c7e508c2558ea
Shivam Bhalla <shivam.bhalla10@gmail.com>
2015-05-07 16:16:47 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/shivam/imagefeed/DetailsActivity.java
@@ -0,0 +1,51 @@
+package com.example.shivam.imagefeed;
+
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class DetailsActivity extends ActionBarActivity {
+
+    ImageView cameraImage;
+    TextView locationText;
+    Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        cameraImage = (ImageView)findViewById(R.id.image);
+        locationText = (TextView)findViewById(R.id.locationText);
+        imageUri = getIntent().getData();
+        Picasso.with(this).load(imageUri.toString()).centerCrop().into(cameraImage);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/shivam/imagefeed/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.shivam.imagefeed;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Uri imageUri;
+    int IMAGE_CONST = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+            Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            imageUri = getOutputUri(IMAGE_CONST);
+            if (imageUri == null) {
+                Toast.makeText(MainActivity.this,
+                        R.string.storage_access_error, Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(pictureIntent, IMAGE_CONST);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Uri getOutputUri(int mediaType) {
+        if (hasExternalStorage()) {
+            // get external storage directory
+            String appName = MainActivity.this.getString(R.string.app_name);
+            File extStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),appName);
+
+            // create subdirectory
+            if(!extStorageDir.exists())
+            {
+                if(!extStorageDir.mkdirs())
+                {
+                    Toast.makeText(MainActivity.this, "Failed to create directory", Toast.LENGTH_SHORT).show();
+                }
+            }
+            File mFile;
+            Date mCurrentDate = new Date();
+            String mTimestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(mCurrentDate);
+            String path = extStorageDir.getPath() + File.separator;
+            if(mediaType == IMAGE_CONST) {
+                mFile = new File(path + "FEEDIMG_" + mTimestamp + ".jpg");
+            }
+            else
+            {
+                return null;
+            }
+            // return the file's URI
+            return Uri.fromFile(mFile);
+        } else {
+            return null;
+        }
+    }
+
+    private boolean hasExternalStorage() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK)
+        {
+            Intent galleryAddIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            galleryAddIntent.setData(imageUri);
+            sendBroadcast(galleryAddIntent);
+
+            Intent sendIntent = new Intent(MainActivity.this,DetailsActivity.class);
+            sendIntent.setData(imageUri);
+            //sendIntent.putExtra("FILE_URI", imageUri);
+            startActivity(sendIntent);
+        }
+        else if(resultCode != RESULT_CANCELED)
+        {
+            Toast.makeText(this, "There was an error", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
