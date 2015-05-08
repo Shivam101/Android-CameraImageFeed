c85da364ffca2211bdaf9a6c0e1968bfeab5d80f
Shivam Bhalla <shivam.bhalla10@gmail.com>
2015-05-08 13:07:39 +0530

    Added Database Structure

--- app/src/main/java/com/example/shivam/imagefeed/DetailsActivity.java
+++ app/src/main/java/com/example/shivam/imagefeed/DetailsActivity.java
@@ -1,5 +1,8 @@
 package com.example.shivam.imagefeed;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
@@ -7,35 +10,83 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class DetailsActivity extends ActionBarActivity {
 
     ImageView cameraImage;
-    TextView locationText;
+    TextView locationText,addressText;
     Uri imageUri;
+    Geocoder geocoder;
+    List<Address> addresses;
+    double latitude,longitude;
+    String resultLatLong,resultAddr;
+    Button mSave,mCancel;
+    PostORM p = new PostORM();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         LocationService service = new LocationService(DetailsActivity.this);
-        AddressCreator addressCreator = new AddressCreator();
         cameraImage = (ImageView)findViewById(R.id.image);
         locationText = (TextView)findViewById(R.id.locationText);
+        addressText = (TextView)findViewById(R.id.addressText);
+        mSave = (Button)findViewById(R.id.savePost);
+        mCancel = (Button)findViewById(R.id.cancelPost);
         imageUri = getIntent().getData();
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p.insertPost(DetailsActivity.this, imageUri.toString(), resultLatLong, resultAddr);
+                Toast.makeText(DetailsActivity.this,"Created new Post",Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(DetailsActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailsActivity.this,"Post Discarded",Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(DetailsActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         Location gpsLocation = service.getLocation(LocationManager.GPS_PROVIDER);
         if (gpsLocation != null) {
-            double latitude = gpsLocation.getLatitude();
-            double longitude = gpsLocation.getLongitude();
-            String result = "Latitude: " + gpsLocation.getLatitude() +
+            latitude = gpsLocation.getLatitude();
+            longitude = gpsLocation.getLongitude();
+            resultLatLong = "Latitude: " + gpsLocation.getLatitude() +
                     " Longitude: " + gpsLocation.getLongitude();
-            locationText.setText(result);
+            geocoder = new Geocoder(this, Locale.getDefault());
+
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String address = addresses.get(0).getAddressLine(0);
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            resultAddr = address+"\n"+city+", "+state;
+            locationText.setText(resultLatLong);
+            addressText.setText(resultAddr);
         }
-           // addressCreator.getAddress(latitude,longitude,DetailsActivity.this,);
 
             Picasso.with(this).load(imageUri.toString()).resize(500,500).into(cameraImage);
 
@@ -50,12 +101,7 @@ public class DetailsActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
---
+++ app/src/main/java/com/example/shivam/imagefeed/PostORM.java
@@ -0,0 +1,109 @@
+package com.example.shivam.imagefeed;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Shivam on 08/05/15 at 12:12 PM.
+ */
+public class PostORM {
+
+    private static final String TAG = "PostORM";
+    public static final String TABLE_NAME = "post";
+    private static final String COLUMN_URI = "uri";
+    private static final String COLUMN_COORDINATES = "coordinates";
+    private static final String COLUMN_ADDRESS = "address";
+    private DatabaseWrapper dw;
+    SQLiteDatabase myDataBase;
+
+    public static final String SQL_CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_URI + " TEXT, "
+                    + COLUMN_COORDINATES + " TEXT, "
+                    + COLUMN_ADDRESS + " TEXT)";
+
+    public static final String SQL_DROP_TABLE =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public int insertPost(Context c,String uri,String coordinates,String address)
+    {
+        DatabaseWrapper databaseWrapper = new DatabaseWrapper(c);
+        myDataBase = databaseWrapper.getWritableDatabase();
+        long postId = 0;
+        if (isDatabaseOpened()) {
+            ContentValues values = new ContentValues();
+            values.put(PostORM.COLUMN_URI,uri);
+            values.put(PostORM.COLUMN_COORDINATES,coordinates);
+            values.put(PostORM.COLUMN_ADDRESS,address);
+            postId = myDataBase.insert(PostORM.TABLE_NAME, "null", values);
+            Log.e(TAG, "Inserted new Post with ID: " + postId);
+            myDataBase.close();
+        }
+        return (int) postId;
+    }
+
+    public ArrayList<String> getUrifromDB(Context c)
+    {
+        DatabaseWrapper databaseWrapper = new DatabaseWrapper(c);
+        myDataBase = databaseWrapper.getWritableDatabase();
+        ArrayList<String> uris = new ArrayList<String>();
+        Cursor cur = myDataBase.rawQuery("SELECT uri from post",null);
+        Log.e("COUNT",String.valueOf(cur.getCount()));
+        for(cur.moveToFirst();!cur.isAfterLast();cur.moveToNext())
+        {
+            uris.add(cur.getString(0));
+        }
+        System.out.println(uris);
+        return uris;
+    }
+
+    public ArrayList<String> getCoordinatesfromDB(Context c)
+    {
+        DatabaseWrapper databaseWrapper = new DatabaseWrapper(c);
+        myDataBase = databaseWrapper.getWritableDatabase();
+        ArrayList<String> coordinates = new ArrayList<String>();
+        Cursor cur = myDataBase.rawQuery("SELECT coordinates from post",null);
+        Log.e("COUNT",String.valueOf(cur.getCount()));
+        for(cur.moveToFirst();!cur.isAfterLast();cur.moveToNext())
+        {
+            coordinates.add(cur.getString(1));
+        }
+        System.out.println(coordinates);
+        return coordinates;
+    }
+
+    public ArrayList<String> getAddressfromDB(Context c)
+    {
+        DatabaseWrapper databaseWrapper = new DatabaseWrapper(c);
+        myDataBase = databaseWrapper.getWritableDatabase();
+        ArrayList<String> addresses = new ArrayList<String>();
+        Cursor cur = myDataBase.rawQuery("SELECT address from post",null);
+        Log.e("COUNT",String.valueOf(cur.getCount()));
+        for(cur.moveToFirst();!cur.isAfterLast();cur.moveToNext())
+        {
+            addresses.add(cur.getString(2));
+        }
+        System.out.println(addresses);
+        return addresses;
+    }
+
+
+    public boolean isDatabaseOpened() {
+        if (myDataBase == null) {
+            return false;
+        } else {
+            return myDataBase.isOpen();
+        }
+    }
+
+
+
+
+
+
+}
